330fdb8e2c12506ab565f78ed3ad036ad52c1ba2
Ryan <jijizaza999@gmail.com>
2020-09-14 10:11:02 -0700

    update

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -25,12 +25,12 @@ class Main extends Component {
 
   render(){
     return (
-   <div className="app">
-      <Header/>
-      <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
-      <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
-      <Footer/>
-   </div>
+      <div>
+        <Header />
+        <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)} />
+        <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+        <Footer />
+    </div>
 
 
     );
