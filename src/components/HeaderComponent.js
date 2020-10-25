faff9631ebe7a673f9a5a6326d57dea4b3e16e5e
Ryan <jijizaza999@gmail.com>
2020-10-25 12:24:18 -0700

    ok

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -56,7 +56,7 @@ class Header extends Component {
 
                 <Navbar dark sticky="top" expand="md">
                     <div className="container">
-                        <NavbarBrand className="mr-auto" href="/home">
+                        <NavbarBrand className="mr-auto" href="/">
                           <img src="https://raw.githubusercontent.com/jijizaza999/json-server/main/images/logo.png" height="30" width="30" alt="NuCamp Logo" />
                         </NavbarBrand>
                         <NavbarToggler onClick={this.toggleNav} />
