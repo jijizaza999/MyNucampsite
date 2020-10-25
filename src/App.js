d22f39a8db287e1aa6bfce12a40c74e2f471dbc1
Ryan <jijizaza999@gmail.com>
2020-10-25 11:57:23 -0700

    router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React,{ Component } from 'react';
 import Main from './components/MainComponent';
-import {HashRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
 import './App.css';
@@ -11,11 +11,11 @@ class App extends Component {
   render(){
     return (
       <Provider store={store}>
-        <HashRouter>
+        <BrowserRouter>
             <div className="App">
                 <Main />
             </div>
-        </HashRouter>
+        </BrowserRouter>
     </Provider>
     );
   }
